b9e0d1013616eaa79f66959bd715c53cc5690dd4
kizarrd <51534324+kizarrd@users.noreply.github.com>
2021-07-26 21:51:39 +0900

    trying gulp settings

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -3,6 +3,7 @@ import del from "del";
 import sass from "gulp-sass";
 import minify from "gulp-csso";
 import autoprefixer from "gulp-autoprefixer";
+import ghPages from "gulp-gh-pages";
 
 sass.compiler = require("node-sass");
 
@@ -11,8 +12,20 @@ const routes = {
     watch: "src/scss/*",
     src: "src/scss/styles1.scss",
     dest: "dist/css"
+  },
+  html: {
+    src: "index.html",
+    dest: "dist"
+  },
+  deploy: {
+    src: "./dist/**/*"
   }
 };
+
+const ghDeploy = () =>
+  gulp
+    .src(routes.deploy.src)
+    .pipe(ghPages());
 
 const styles = () =>
   gulp
@@ -27,16 +40,22 @@ const styles = () =>
     .pipe(minify())
     .pipe(gulp.dest(routes.css.dest));
 
+const template = () =>
+    gulp
+      .src(routes.html.src)
+      .pipe(gulp.dest(routes.html.dest));
+
 const watch = () => {
   gulp.watch(routes.css.watch, styles);
 };
 
-const clean = () => del(["dist/"]);
+const clean = () => del(["dist/", ".publish/"]);
 
 const prepare = gulp.series([clean]);
 
-const assets = gulp.series([styles]);
+const assets = gulp.series([styles, template]);
 
 const live = gulp.parallel([watch]);
 
 export const dev = gulp.series([prepare, assets, live]);
+export const deploy = gulp.series([prepare, assets, ghDeploy]);
